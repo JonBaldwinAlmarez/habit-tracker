0130b753e5a1c20031d66d2b27da0b4675b2b2d1
JonBaldwinAlmarez <almarezjbm@gmail.com>
2026-05-07 17:18:18 +0800

    Feature: Add Component Header

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import { type ReactNode } from "react";
+
+interface ButtonProps {
+	children: ReactNode;
+}
+
+const Button = ({ children }: ButtonProps) => {
+	return (
+		<button className="bg-red-600 hover:bg-red-400 transition-colors rounded py-1 px-2 disabled:opacity-30 disabled:cursor-not-allowed">
+			{children}
+		</button>
+	);
+};
+
+export default Button;
